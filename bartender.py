c85798cbb25eaf1f08d5afbf6132ba24bd923809
drewbrinkley <drewbrinkley24@gmail.com>
2022-01-29 14:57:30 -0500

    created have_drinks() function to validate user's 1st choice

--- bartender.py
+++ bartender.py
@@ -6,14 +6,14 @@ import random
 # define function 'age()' to collect and validate user's age
 def age():
     print("age() has been called") ### Debugging prompt - delete in final code
-    # assign user input to variable 'age'
+    # assign user input to variable 'age_input'
     age_input = input("Please enter your age, in years:\n> ")
     
     print(f">>> {age_input}")  ### Debugging prompt - delete in final code
     
-    # use try...except block to vailidate user input
+    # use try...except block to vailidate that user input is a number
     try:
-        # tests to see if the 'age' value can be converted to an integer
+        # tests to see if the 'age_input' value can be converted to an integer
         int(age_input) 
         # assigns True to variable 'it_is' if conversion is successful
         it_is = True
@@ -23,22 +23,70 @@ def age():
 
     print(it_is)  ### debugging prompt - delete in final code
     
-    # if-statement to confirm validity of user input for 'age'
+    # if-statement to confirm validity of user input for 'age_input'
+    # if executes associated code block when 'age_input' succesfully converted to integer
     if it_is == True:
         # print string to confirm user entered a number when prompted
-        print("You have entered a valid age.")
+        print("Thank you for entering your age.")
         # create global variable 'age_num' for use in other functions
         global age_num
-        # convert 'age_value' to integer and assign to 'age'
+        # convert 'age_value' to integer and assign to 'age_num'
         age_num = int(age_input)
-        print(f"---{age_num}")
+        print(f"---{age_num}")  ### debugging statement - deleted in final code
+        # return value of 'age_num' for use elsewhere
         return age_num
-        
+    # else-statement that will run when 'age_input' fails conversion to integer     
     else:
         # print string to prompt user to re-enter age
         print("You did not enter a valid age.  Please try again.")
         # call age() function again to loop user back to 'age' input()
         age()
+
+
+# define function 'have_drinks()' to get user input on initial drink decision
+def have_drinks():
+    print("have_drinks() has been called") ### Debugging prompt - delete in final code
+    # assign user input to variable 'choice_input'
+    choice_input = input(f"""Type one of the following numbers to tell me what you would like to do:
+        1 - Stay home and have some drinks
+        2 - Go to a bar for some drinks
+        3 - I don't feel like drinking today
+        > """)
+        
+    print(f">>> {choice_input}")  ### Debugging prompt - delete in final code
+    
+    # use try...except block to vailidate user input
+    try:
+        # tests to see if the 'choice_input' value can be converted to an integer
+        int(choice_input)
+        # assigns True to variable 'it_is' if conversion is successful
+        it_is = True
+    except ValueError:
+        # assigns False to variable 'it_is' if conversion returns an error (user did not enter a number)
+        it_is = False
+
+    print(it_is)  ### debugging prompt - delete in final code
+    
+    # if-statement to confirm validity of user input for 'choice_input'
+    # code for if will run when user input a 1, 2, or 3
+    if it_is == True and (0 < int(choice_input) < 4):
+        # ----- print string to debug - delete in final code
+        print("pass.")
+        # create global variable 'choice' for use in other functions
+        global choice 
+        # convert 'choice_input' to integer and assign to 'choice'
+        choice = int(choice_input)
+        print(f"---{choice}") #### debugging print - delete in final code
+        # return value for 'choice' to use elsewhere
+        return choice
+    # else will run when user input was invalid (not a number or not in range)    
+    else:
+        # print string to prompt user to re-enter selection
+        print("You did not enter a valid selection.  Please try again.")
+        # call have_drinks() function again to loop user back to try again
+        have_drinks()
+    
+    print("exit have_drinks()")
 
 
 # define start() function that will be called to begin the game
@@ -56,7 +104,15 @@ def start():
     else:
         # print string 
         print("next step...")
-        # call start() function again to loop user back to 'age' input()
+        have_drinks()
+
+        if choice == 1:
+            print("call home()")
+        elif choice == 2:
+            print("call bar()")
+        else:
+            print("call hangover()")
+        
         
 
     print("<<<< exit start()")  ### Debugging prompt - delete in final code
